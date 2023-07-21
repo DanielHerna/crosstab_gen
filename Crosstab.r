ad0730e787ce44362467e4c4801d4275252c671c
DanielHerna <Daniel.Hernandez@conjointly.com>
2023-07-21 15:39:12 -0500

    new changes to the code including the format Kirill metioned

--- Crosstab.r
+++ Crosstab.r
@@ -1,32 +1,37 @@
 setwd('C:/Users/DH/Documents/Projects/Apps/Crosstab')
 
-package.list <- c("readxl","tidyverse")
+package.list <- c("readxl","tidyverse","tidyr")
 new.packages <- package.list[!(package.list %in% installed.packages()[,"Package"])]
 if (length(new.packages)) install.packages(new.packages)
 
 library('readxl')
 library('tidyverse')
+library('tidyr')
 
 data <- read_xlsx('UnitC1.xlsx',sheet='data')
 segments <- read_xlsx('UnitC1.xlsx',sheet='Segment membership')
-merged_table <- merge(data,segments,by='participant_id')
 
-#View(merged_table)
-#dim(data)
-#dim(segments)
-#dim(merged_table)
+crosstab <- function(data,segments){
 
-segments = unique(merged_table$name)
+  merged_table <- merge(data,segments,by='participant_id')
 
-remove_special_characters <- function(text) {
-  gsub("[^[:alnum:][:space:]]", "", text)
+  merged_table <- select(merged_table,-c(status_included_in_current_report,
+  segment_id))
+
+  data_stacked <- pivot_longer(merged_table, cols = -c(name,participant_id), names_to = "variable", values_to = "value")
+
+  final_data <- data_stacked%>%group_by(name,variable)%>%summarize(median(value))
+  final_data <- t(final_data)
+
+  overall <- data_stacked%>%group_by(variable)%>%summarize(median(value))
+  overall <- rbind("Total",t(overall)) 
+
+  final_data <<- cbind(final_data,overall)
+  return(final_data)
+
 }
 
-# Apply the function to the segments vector
-cleaned_segments <- sapply(segments, remove_special_characters)
+data_test <- crosstab(data = data,segments = segments)
+View(data_test)
 
-for (segment in cleaned_segments) {
-
-   crostabb_seg <- merged_table%>%filter(name==segment)
-   write.csv(crostabb_seg,paste0('Crosstab ',segment,'.csv'))
-}
+write.csv(data_test,'data_test.csv')
